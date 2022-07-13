3f22a43270dd8f70d1da41a5e897043859e03bea
konstantin <nkp.yalta@mail.ru>
2022-07-13 19:26:25 +0300

    exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNatural(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+
+    PrintNatural(m, n - 2);
+    System.Console.Write($"{n} ");
+}
+
+int m = Prompt("Введите число М => ");
+int n = Prompt("Введите число N => ");
+if (n % 2 == 1) n--;
+
+PrintNatural(m, n);
